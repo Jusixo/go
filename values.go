69184a1c76f6aeec7b2f15f77bc57f0570c5b7e6
Justin Valentin <Justinvalentine88@gmail.com>
2017-07-26 19:03:40 -0400

    testing values

---
+++ values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main()  {
+  fmt.Println("go" + "lang")
+
+  fmt.Println("2+1 =", 2+1)
+  fmt.Println("10.0/5.0=", 10.0/5.0)
+
+  fmt.Println(true && false)
+  fmt.Println(true || false)
+  fmt.Println(!true)
+}
